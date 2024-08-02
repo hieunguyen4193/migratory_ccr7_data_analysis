767c14176e49a6fd93c0f66dba7ab1366eb512b0
hieunguyen4193 <work@hieunguyen.de>
2024-08-02 16:29:17 +0200

    update

--- convert_seurat_to_anndata_for_RNA_velocity_analysis.R
+++ convert_seurat_to_anndata_for_RNA_velocity_analysis.R
@@ -8,48 +8,77 @@ source(file.path(path.to.project.src, "import_libraries.R"))
 source(file.path(path.to.project.src, "helper_functions.R"))
 source(file.path(path.to.project.src, "config.R"))
 
+all.datasets <- c("220907_FH",
+                  "GSM5764259", 
+                  "integrate_GSE192742_LIVER",
+                  "230228_FH",
+                  "GSM5764288",            
+                  "GSM5764245",
+                  "gutcellatlas_myeloid"
+)
+
 outdir <- "/media/hieunguyen/HNSD01/outdir"
-orig.dataset <- "GSM5764245"
+
+# orig.dataset <- "GSM5764245"
+
 config.version <- "v0.1"
 output.version <- "20240723"
 PROJECT <- "FHager_datasets"
 
 input.config <- config.params[[config.version]]
-dataset.name <- sprintf("%s_%s", orig.dataset, config.version)
 
-path.to.main.input <- file.path(outdir,
-                                PROJECT,
-                                output.version, 
-                                dataset.name, 
-                                "s8a_output",
-                                sprintf("%s.output.s8a.rds", dataset.name))
+for (orig.dataset in all.datasets){
+  print(sprintf("working on dataset %s", orig.dataset))
+  dataset.name <- sprintf("%s_%s", orig.dataset, config.version)
+  path.to.seurat2anndata <- file.path(outdir, PROJECT, output.version, "seurat2anndata", dataset.name)
+  dir.create(path.to.seurat2anndata, showWarnings = FALSE, recursive = TRUE)
+  if (file.exists(file.path(path.to.seurat2anndata, sprintf('finished_%s.csv', dataset.name))) == FALSE){
+    if (orig.dataset == "integrate_GSE192742_LIVER"){
+      path.to.main.input <- file.path(outdir,
+                                      PROJECT,
+                                      output.version, 
+                                      dataset.name, 
+                                      "s8_output",
+                                      sprintf("%s.output.s8.rds", dataset.name))
+    } else {
+      path.to.main.input <- file.path(outdir,
+                                      PROJECT,
+                                      output.version, 
+                                      dataset.name, 
+                                      "s8a_output",
+                                      sprintf("%s.output.s8a.rds", dataset.name))
+    }
+    
+    
 
-path.to.seurat2anndata <- file.path(outdir, PROJECT, output.version, "seurat2anndata", dataset.name)
-dir.create(path.to.seurat2anndata, showWarnings = FALSE, recursive = TRUE)
-
-s.obj <- readRDS(path.to.main.input)
-
-object.name <- dataset.name
-
-s.obj$barcode <- colnames(s.obj)
-
-if (dataset.name = sprintf("integrate_GSE192742_LIVER_%s", config.version)){
-  s.obj$UMAP_1 <- s.obj@reductions$INTE_UMAP@cell.embeddings[,1]
-  s.obj$UMAP_2 <- s.obj@reductions$INTE_UMAP@cell.embeddings[,2]
-} else {
-  s.obj$UMAP_1 <- s.obj@reductions$RNA_UMAP@cell.embeddings[,1]
-  s.obj$UMAP_2 <- s.obj@reductions$RNA_UMAP@cell.embeddings[,2]
+    
+    s.obj <- readRDS(path.to.main.input)
+    
+    s.obj$barcode <- colnames(s.obj)
+    
+    if (dataset.name == sprintf("integrate_GSE192742_LIVER_%s", config.version)){
+      s.obj$UMAP_1 <- s.obj@reductions$INTE_UMAP@cell.embeddings[,1]
+      s.obj$UMAP_2 <- s.obj@reductions$INTE_UMAP@cell.embeddings[,2]
+      write.csv(s.obj@reductions$INTE_UMAP@cell.embeddings, file=file.path(path.to.seurat2anndata, sprintf('pca_%s.csv', dataset.name)), quote=F, row.names=F)
+      write.csv(s.obj@meta.data, file=file.path(path.to.seurat2anndata, sprintf('metadata_%s.csv', dataset.name)), quote=F, row.names=F)
+    } else {
+      s.obj$UMAP_1 <- s.obj@reductions$RNA_UMAP@cell.embeddings[,1]
+      s.obj$UMAP_2 <- s.obj@reductions$RNA_UMAP@cell.embeddings[,2]
+      write.csv(s.obj@reductions$RNA_UMAP@cell.embeddings, file=file.path(path.to.seurat2anndata, sprintf('pca_%s.csv', dataset.name)), quote=F, row.names=F)
+      write.csv(s.obj@meta.data, file=file.path(path.to.seurat2anndata, sprintf('metadata_%s.csv', dataset.name)), quote=F, row.names=F)
+    }
+    
+    # write expression counts matrix
+    library(Matrix)
+    counts_matrix <- GetAssayData(s.obj, assay='RNA', slot='counts')
+    writeMM(counts_matrix, file=file.path(path.to.seurat2anndata, sprintf('counts_%s.mtx', dataset.name)))
+    
+    # write gene names
+    write.table( data.frame('gene'=rownames(counts_matrix)),file=file.path(path.to.seurat2anndata, sprintf('gene_names_%s.csv', dataset.name)),
+                 quote=F,row.names=F,col.names=F)
+    write.csv(data.frame(status = c("finished converting for the dataset")), file.path(path.to.seurat2anndata, sprintf('finished_%s.csv', dataset.name)))
+  } else {
+    print(sprintf("File %s exists", file.path(path.to.seurat2anndata, sprintf('finished_%s.csv', dataset.name))))
+  }
 }
 
-# write dimesnionality reduction matrix, in this example s.obj.case pca matrix
-write.csv(s.obj@reductions$INTE_UMAP@cell.embeddings, file=file.path(path.to.seurat2anndata, sprintf('pca_%s.csv', object.name)), quote=F, row.names=F)
-write.csv(s.obj@meta.data, file=file.path(path.to.seurat2anndata, sprintf('metadata_%s.csv', object.name)), quote=F, row.names=F)
-
-# write expression counts matrix
-library(Matrix)
-counts_matrix <- GetAssayData(s.obj, assay='RNA', slot='counts')
-writeMM(counts_matrix, file=file.path(path.to.seurat2anndata, sprintf('counts_%s.mtx', object.name)))
-
-# write gene names
-write.table( data.frame('gene'=rownames(counts_matrix)),file=file.path(path.to.seurat2anndata, sprintf('gene_names_%s.csv', object.name)),
-             quote=F,row.names=F,col.names=F)
---
+++ run_to_generate_02_html.R
@@ -0,0 +1,52 @@
+##### clean up #####
+gc()
+rm(list = ls())
+
+path.to.project.src <- "/media/hieunguyen/HNSD01/src/migratory_ccr7_data_analysis"
+
+source(file.path(path.to.project.src, "import_libraries.R"))
+source(file.path(path.to.project.src, "helper_functions.R"))
+source(file.path(path.to.project.src, "config.R"))
+
+#####----------------------------------------------------------------------#####
+# CONFIGURATIONS AND PREPRATIONS
+#####----------------------------------------------------------------------#####
+outdir <- "/media/hieunguyen/HNSD01/outdir"
+
+config.version <- "v0.1"
+output.version <- "20240723"
+PROJECT <- "FHager_datasets"
+
+input.config <- config.params[[config.version]]
+
+# path.to.save.html <- file.path(outdir, PROJECT, output.version, "html_outputs", "02_output")
+path.to.save.html <- file.path(outdir, PROJECT, output.version, "html_outputs", "02_output_monocle_2.32.0")
+dir.create(path.to.save.html, showWarnings = FALSE, recursive = TRUE)
+
+#####----------------------------------------------------------------------#####
+##### 01 html
+#####----------------------------------------------------------------------#####
+all.datasets <- c("220907_FH",
+                  "GSM5764259", 
+                  "integrate_GSE192742_LIVER",
+                  "230228_FH",
+                  "GSM5764288",            
+                  "GSM5764245",
+                  "gutcellatlas_myeloid"
+)
+for (orig.dataset in all.datasets){
+  print(sprintf("working on the dataset %s", orig.dataset))
+  path.to.rmd <- file.path(path.to.project.src, "02_trajectory_analysis_with_monocle2.Rmd")
+  save.html.name <- sprintf("%s_%s.html", orig.dataset, config.version)
+  if (file.exists(file.path(path.to.save.html, save.html.name)) == FALSE){
+    rmarkdown::render(path.to.rmd, params = list(
+      config.version = config.version,
+      orig.dataset = orig.dataset,
+      output.version = output.version
+    ),
+    output_file = save.html.name, 
+    output_dir = path.to.save.html)
+  } else {
+    print(sprintf("The file %s already exists at %s", save.html.name, path.to.save.html))
+  }
+}
