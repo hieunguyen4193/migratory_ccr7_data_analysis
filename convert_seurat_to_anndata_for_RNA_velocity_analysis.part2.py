3db433117a78653ed8954bae9ab83c2dcff1f9b7
hieunguyen4193 <work@hieunguyen.de>
2024-08-07 15:06:34 +0200

    update

--- convert_seurat_to_anndata_for_RNA_velocity_analysis.part2.py
+++ convert_seurat_to_anndata_for_RNA_velocity_analysis.part2.py
@@ -25,51 +25,61 @@ import warnings
 warnings.filterwarnings("ignore")
 
 outdir = "/media/hieunguyen/HNSD01/outdir"
-orig_dataset = "GSM5764245"
-config_version = "v0.1"
-output_version = "20240806"
-PROJECT = "FHager_datasets"
+# orig_dataset = "GSM5764245"
+all_datasets = ["220907_FH",
+                "GSM5764259",
+                "230228_FH",
+                "GSM5764288",            
+                "GSM5764245",
+                "integrate_GSE192742_LIVER",
+                "gutcellatlas_myeloid"]
 
-dataset_name = "{}_{}".format(orig_dataset, config_version)
-
-path_to_main_input = os.path.join(outdir,
-                                PROJECT,
-                                output_version, 
-                                dataset_name, 
-                                "s8a_output",
-                                "{}.output.s8a.rds".format(dataset_name))
-
-path_to_seurat2anndata = os.path.join(outdir, PROJECT, output_version, "seurat2anndata", dataset_name)
-
-#####---------------------------------------------------------------------------------------------------------#####
-# load sparse matrix:
-sample_name = dataset_name
-X = io.mmread(os.path.join(path_to_seurat2anndata, "counts_{}.mtx".format(sample_name)))
-
-# create anndata object
-adata = anndata.AnnData(X=X.transpose().tocsr())
-
-# load cell metadata:
-cell_meta = pd.read_csv(os.path.join(path_to_seurat2anndata, "metadata_{}.csv".format(sample_name)))
-
-# load gene names:
-with open(os.path.join(path_to_seurat2anndata, "gene_names_{}.csv".format(sample_name)), 'r') as f:
-  gene_names = f.read().splitlines()
-# set anndata observations and index obs by barcodes, var by gene names
-adata.obs = cell_meta
-adata.obs.index = adata.obs['barcode']
-adata.var.index = gene_names
-
-# load dimensional reduction:
-pca = pd.read_csv(os.path.join(path_to_seurat2anndata, "pca_{}.csv".format(sample_name)))
-pca.index = adata.obs.index
-
-# set pca and umap
-adata.obsm['X_pca'] = pca.to_numpy()
-adata.obsm['X_umap'] = np.vstack((adata.obs['UMAP_1'].to_numpy(), adata.obs['UMAP_2'].to_numpy())).T
-
-# save dataset as anndata format
-adata.write(os.path.join(path_to_seurat2anndata, '{}.h5ad'.format(sample_name)))
-
-# reload dataset
-adata = sc.read_h5ad(os.path.join(path_to_seurat2anndata, '{}.h5ad'.format(sample_name)))
+for orig_dataset in tqdm(all_datasets):
+    config_version = "v0.1"
+    output_version = "20240806"
+    PROJECT = "FHager_datasets"
+    
+    dataset_name = "{}_{}".format(orig_dataset, config_version)
+    
+    path_to_main_input = os.path.join(outdir,
+                                    PROJECT,
+                                    output_version, 
+                                    dataset_name, 
+                                    "s8a_output",
+                                    "{}.output.s8a.rds".format(dataset_name))
+    
+    path_to_seurat2anndata = os.path.join(outdir, PROJECT, output_version, "seurat2anndata", dataset_name)
+    
+    #####---------------------------------------------------------------------------------------------------------#####
+    # load sparse matrix:
+    sample_name = dataset_name
+    X = io.mmread(os.path.join(path_to_seurat2anndata, "counts_{}.mtx".format(sample_name)))
+    
+    # create anndata object
+    adata = anndata.AnnData(X=X.transpose().tocsr())
+    
+    # load cell metadata:
+    cell_meta = pd.read_csv(os.path.join(path_to_seurat2anndata, "metadata_{}.csv".format(sample_name)))
+    
+    # load gene names:
+    with open(os.path.join(path_to_seurat2anndata, "gene_names_{}.csv".format(sample_name)), 'r') as f:
+      gene_names = f.read().splitlines()
+    # set anndata observations and index obs by barcodes, var by gene names
+    adata.obs = cell_meta
+    adata.obs.index = adata.obs['barcode']
+    adata.var.index = gene_names
+    
+    # load dimensional reduction:
+    pca = pd.read_csv(os.path.join(path_to_seurat2anndata, "pca_{}.csv".format(sample_name)))
+    pca.index = adata.obs.index
+    
+    # set pca and umap
+    adata.obsm['X_pca'] = pca.to_numpy()
+    adata.obsm['X_umap'] = np.vstack((adata.obs['UMAP_1'].to_numpy(), adata.obs['UMAP_2'].to_numpy())).T
+    
+    # save dataset as anndata format
+    print("writting file adata to {}".format(os.path.join(path_to_seurat2anndata, '{}.h5ad'.format(sample_name))))
+    adata.write(os.path.join(path_to_seurat2anndata, '{}.h5ad'.format(sample_name)))
+    
+    # reload dataset
+    adata = sc.read_h5ad(os.path.join(path_to_seurat2anndata, '{}.h5ad'.format(sample_name)))
---
+++ generate_color_lists.R
@@ -0,0 +1,53 @@
+##### clean up #####
+gc()
+rm(list = ls())
+
+path.to.project.src <- "/media/hieunguyen/HNSD01/src/migratory_ccr7_data_analysis"
+
+source(file.path(path.to.project.src, "import_libraries.R"))
+source(file.path(path.to.project.src, "helper_functions.R"))
+source(file.path(path.to.project.src, "config.R"))
+
+all.datasets <- c("220907_FH",
+                  "GSM5764259", 
+                  "integrate_GSE192742_LIVER",
+                  "230228_FH",
+                  "GSM5764288",            
+                  "GSM5764245",
+                  "gutcellatlas_myeloid"
+)
+
+outdir <- "/media/hieunguyen/HNSD01/outdir"
+
+config.version <- "v0.1"
+output.version <- "20240806"
+PROJECT <- "FHager_datasets"
+
+input.config <- config.params[[config.version]]
+
+for (orig.dataset in all.datasets){
+  print(sprintf("working on dataset %s", orig.dataset))
+  dataset.name <- sprintf("%s_%s", orig.dataset, config.version)
+  path.to.save.colors <- file.path(outdir, PROJECT, output.version, "colors", dataset.name)
+  dir.create(path.to.save.colors, showWarnings = FALSE, recursive = TRUE)
+    if (orig.dataset == "integrate_GSE192742_LIVER"){
+      path.to.main.input <- file.path(outdir,
+                                      PROJECT,
+                                      output.version, 
+                                      dataset.name, 
+                                      "s8_output",
+                                      sprintf("%s.output.s8.rds", dataset.name))
+    } else {
+      path.to.main.input <- file.path(outdir,
+                                      PROJECT,
+                                      output.version, 
+                                      dataset.name, 
+                                      "s8a_output",
+                                      sprintf("%s.output.s8a.rds", dataset.name))
+    }
+    s.obj <- readRDS(path.to.main.input)
+    num.clusters <- length(unique(s.obj$seurat_clusters))
+    library(scales)
+    colors <- hue_pal()(num.clusters)
+    write.csv(data.frame(color = colors), file.path(path.to.save.colors, "colordf.csv"))
+}
